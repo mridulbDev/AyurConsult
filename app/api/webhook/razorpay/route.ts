f5b6485bd15e0b5c0b593ff51a69a7648b65618f
mridulbDev <mridulb.dev@gmail.com>
2026-02-05 22:55:21 +0530

    finalised

--- app/api/webhook/razorpay/route.ts
+++ app/api/webhook/razorpay/route.ts
@@ -13,10 +13,11 @@ export async function POST(req: Request) {
     if (signature !== expectedSignature) return new Response('Unauthorized', { status: 400 });
     console.log("Razorpay Signature Verified");
     const data = JSON.parse(body);
-    if (data.event !== 'payment.captured' && data.event !== 'order.paid') return new Response('OK');
+    if (data.event !== 'payment.captured' ||  data.event !== 'order.paid') return new Response('OK');
 
     const notes = data.payload.payment?.entity?.notes || data.payload.order?.entity?.notes;
     const bookingId = notes?.booking_id;
+    console.log("Razor Pay Processing Booking ID:", bookingId);
     const auth = new google.auth.JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL!,
       key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
